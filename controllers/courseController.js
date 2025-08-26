e597e36630584faabb00bbb17b2c4ff4e242d04a
Lakshan Dissanayake <IT20197414@my.sliit.lk>
2025-08-27 00:38:35 +0530

    Fetching a Single Course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -19,4 +19,35 @@ const createCourse = asyncHandler(async (req, res) => {
   res.status(201).json(createdCourse);
 });
 
-export { createCourse };
+
+// @desc    Fetch all courses
+// @route   GET /api/courses
+// @access  Public
+const getCourses = asyncHandler(async (req, res) => {
+  // We use .populate('instructor', 'name') to fetch the instructor's name
+  // instead of just their ID. This is a powerful Mongoose feature.
+  const courses = await Course.find({}).populate('instructor', 'name');
+  res.json(courses);
+});
+
+
+
+// @desc    Fetch a single course by ID
+// @route   GET /api/courses/:id
+// @access  Public
+const getCourseById = asyncHandler(async (req, res) => {
+  const course = await Course.findById(req.params.id).populate(
+    'instructor',
+    'name email' // We can ask for more fields from the instructor
+  );
+
+  if (course) {
+    res.json(course);
+  } else {
+    res.status(404);
+    throw new Error('Course not found');
+  }
+});
+
+
+export { createCourse, getCourses, getCourseById };
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
 const router = express.Router();
-import { createCourse } from '../controllers/courseController.js';
 import { protect, instructor } from '../middleware/authMiddleware.js';
+import { createCourse, getCourses,getCourseById, } from '../controllers/courseController.js';
 
 // The route for creating a course.
 // We apply 'protect' first, then 'instructor' middleware.
 // Only if both pass will the createCourse controller function run.
 router.route('/').post(protect, instructor, createCourse);
+
+
+// Route for getting all courses (public) and creating a course (private)
+router.route('/')
+  .get(getCourses) // 2. ADD the GET handler
+  .post(protect, instructor, createCourse);
+
+// Route for '/:id'
+router.route('/:id').get(getCourseById);  
 
 export default router;
